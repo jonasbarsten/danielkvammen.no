9302006a713cc7c9e23385363d023259974d171d
Jonas Barsten <jonasbarsten@gmail.com>
2018-05-12 20:21:47 +0200

    Styling bandsintown events

--- client/front/components/artists/ArtistEventsBIT.js
+++ client/front/components/artists/ArtistEventsBIT.js
@@ -5,7 +5,7 @@ class ArtistEventsBIT extends Component {
 	render () {
 		return (
 			<div>
-				<a className="bit-widget-initializer" data-artist-name="Daniel Kvammen" data-display-local-dates="false" data-display-past-dates="true" data-auto-style="false" data-text-color="#000000" data-link-color="#2F95DE" data-popup-background-color="#FFFFFF" data-background-color="#FFFFFF" data-display-limit="" data-link-text-color="#FFFFFF"></a>
+				<a className="bit-widget-initializer" data-artist-name="Daniel Kvammen" data-display-local-dates="false" data-display-past-dates="false" data-auto-style="true" data-display-limit="" data-display-logo="false" data-display-past-dates="false" data-widget-width="100%"></a>
 			</div>
 		);
 	}
